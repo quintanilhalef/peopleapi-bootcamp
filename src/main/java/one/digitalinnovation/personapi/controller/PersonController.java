6942a672a8bfafd7aa317431e78485b1775c9b57
quintanilhalef <quintanilhalef@gmail.com>
2021-08-25 12:04:20 -0300

    Adicionado metodo listAll e createPerson

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,34 +1,31 @@
 package one.digitalinnovation.personapi.controller;
 
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
-import one.digitalinnovation.personapi.entities.Person;
-import one.digitalinnovation.personapi.repository.PersonRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import one.digitalinnovation.personapi.services.PersonService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    private PersonRepository personRepository;
-
-    @Autowired
-    public PersonController(PersonRepository personRepository){
-        this.personRepository = personRepository;
-    }
+    private PersonService personService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public MessageResponseDTO createPerson(@RequestBody Person person) {
-        Person savedPerson = personRepository.save(person);
-        return MessageResponseDTO
-                .builder()
-                .message("Created person with ID: "+savedPerson.getId())
-                .build();
-
+    public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
+        return personService.createPerson(personDTO);
     }
 
+
+    @GetMapping
+    public List<PersonDTO> listAll(){
+        return personService.listAll();
     }
+}
 
 
---
+++ src/main/java/one/digitalinnovation/personapi/services/PersonService.java
@@ -0,0 +1,41 @@
+package one.digitalinnovation.personapi.services;
+
+import one.digitalinnovation.personapi.dto.mapper.PersonMapper;
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
+import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.personapi.entities.Person;
+import one.digitalinnovation.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PersonService {
+
+    private PersonRepository personRepository;
+    private PersonMapper personMapper;
+    @Autowired
+    public PersonService(PersonRepository personRepository){
+        this.personRepository = personRepository;
+    }
+
+    public MessageResponseDTO createPerson(@RequestBody PersonDTO personDTO) {
+        Person personToSave = personMapper.toModel(personDTO);
+
+        Person savedPerson = personRepository.save(personToSave);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID: "+savedPerson.getId())
+                .build();
+
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
+
+    }
+}
